87b029bd75b40018d10df702caeabaaf3ef2fafa
vvh0ame <77536370+vvh0ame@users.noreply.github.com>
2024-10-11 22:35:30 +0500

    Update and rename quokka_pics.py to quokka_pics.js

---
+++ src/quokka_pics.js
@@ -0,0 +1,19 @@
+class QuokkaPics {
+	constructor() {
+		this.api = "https://quokka.pics"
+		this.headers = {
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+		}
+	}
+
+	async getRandomImage() {
+		const response = await fetch(
+			`${this.api}/api`, {
+				method: "GET",
+				headers: this.headers
+			})
+		return response.json()
+	}
+ }
+
+module.exports = {QuokkaPics}
